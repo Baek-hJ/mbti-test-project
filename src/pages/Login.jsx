f266c5895b4e533cf587394fbdd12eb4cdf9efe8
hj <hyeonjubaek06@gmail.com>
2025-02-21 17:44:18 +0900

    Add: 로그인 페이지에 폼 요소 추가

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 const Login = () => {
   return (
     <div>
+      <form action="">
       <h1>로그인</h1>
       <input type="text" placeholder="아이디" />
       <input type="password" placeholder="비밀번호" />
@@ -12,6 +13,7 @@ const Login = () => {
         }}>
         로그인
       </button>
+      </form>
       <h2>계정이 없으신가요?</h2>
       <Link to="/Signup">회원가입</Link>
     </div>
