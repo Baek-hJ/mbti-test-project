be79da3f99fe715f99a3b120ab2929083f3ffcf6
hj <hyeonjubaek06@gmail.com>
2025-02-21 16:50:12 +0900

    Add: Signup, Login, Profile, Results, Test 라우트 추가

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from '../pages/Home';
+import Signup from '../pages/Signup';
+import Login from '../pages/login';
+import Profile from '../pages/Profile';
+import Results from '../pages/results';
+import Test from '../pages/test';
 
 const Router = () => {
   return (
@@ -8,6 +12,11 @@ const Router = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path='/Signup' element={<Signup/>} />
+          <Route path='/Login' element={<Login />} />
+          <Route path='Profile' element={<Profile/>} />
+          <Route path='Results' element={<Results />} />
+          <Route path='Test' element={<Test />} />
         </Routes>
       </BrowserRouter>
     </div>
