eb167dd8c29793a95771a64dd714997761eaeb7c
hj <hyeonjubaek06@gmail.com>
2025-02-21 17:47:04 +0900

    Add: 프로필 페이지에 닉네임 수정 입력란 및 수정 버튼 추가

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,8 +1,18 @@
 
 const Profile = () => {
     return (
-        <div>Profile</div>
+        <div>
+            <h1>프로필</h1>
+            <input type="text" placeholder="닉네임 수정" />
+            <button
+                onClick={() => {
+                    alert('수정되었습니다.');
+                }}
+            >
+                수정
+            </button>
+        </div>
     )
 }
 
-export default Profile
+export default Profile;
