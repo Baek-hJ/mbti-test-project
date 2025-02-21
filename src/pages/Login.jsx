d01047ea0e2ffc200207db61b7c766979102a7b8
hj <hyeonjubaek06@gmail.com>
2025-02-21 17:39:11 +0900

    Add: 로그인 페이지 UI 및 회원가입 링크 추가

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,20 @@
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div>login</div>
+    <div>
+      <h1>로그인</h1>
+      <input type="text" placeholder="아이디" />
+      <input type="password" placeholder="비밀번호" />
+      <button
+        onClick={() => {
+          alert("로그인 되었습니다.");
+        }}>
+        로그인
+      </button>
+      <h2>계정이 없으신가요?</h2>
+      <Link to="/Signup">회원가입</Link>
+    </div>
   )
 }
 
