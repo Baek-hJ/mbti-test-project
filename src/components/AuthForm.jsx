4c88a4de9324fbb590ee3ec3f8a6468a94c3697f
hj <hyeonjubaek06@gmail.com>
2025-02-25 01:57:38 +0900

    Add: 회원가입을 위한 입력 필드 및 버튼 추가

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -1,8 +1,19 @@
 
 const AuthForm = () => {
     return (
-        <div>AuthForm</div>
+        <div>      
+            <input type="text" placeholder="아이디" />
+            <input type="password" placeholder="비밀번호" />
+            <input type="text" placeholder="닉네임" />
+            <button
+                onClick={() => {
+                    alert("회원가입 되었습니다.");
+                }}
+            >
+                회원가입
+            </button>
+            </div>
     )
 }
 
-export default AuthForm
+export default AuthForm;
