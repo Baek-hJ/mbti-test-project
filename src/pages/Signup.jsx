2e3fc5e32e87304f1ef8955c373566b47ec75141
hj <hyeonjubaek06@gmail.com>
2025-02-25 01:58:03 +0900

    Remove: 회원가입 폼의 불필요한 입력 필드 및 버튼 삭제

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -5,16 +5,6 @@ const Signup = () => {
     <div>
       <form action="">
       <h1>회원가입</h1>
-      <input type="text" placeholder="아이디" />
-      <input type="password" placeholder="비밀번호" />
-      <input type="text" placeholder="닉네임" />
-      <button
-        onClick={() => {
-          alert("회원가입 되었습니다.");
-        }}
-      >
-        회원가입
-      </button>
       </form>
       <h2>이미 계정이 있으신가요?</h2>
       <Link to="/Login">로그인</Link>
