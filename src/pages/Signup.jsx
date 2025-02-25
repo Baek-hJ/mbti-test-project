b63a8d97b5bcd452888e35d7d429e866f1840a69
hj <hyeonjubaek06@gmail.com>
2025-02-25 11:02:29 +0900

    Refactor: Signup 컴포넌트의 UI 개선 및 AuthForm 통합

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,15 +1,21 @@
 import { Link } from "react-router-dom";
+import AuthForm from "../components/AuthForm";
 
 const Signup = () => {
   return (
-    <div>
-      <form action="">
-      <h1>회원가입</h1>
-      </form>
-      <h2>이미 계정이 있으신가요?</h2>
-      <Link to="/Login">로그인</Link>
+    <div className="w-full max-w-lg mx-auto p-6 bg-gray-50 rounded-lg shadow-lg">
+      <h2 className="text-3xl font-semibold text-center text-gray-800 mb-4">
+        회원가입
+      </h2>
+      <AuthForm isSignup={true} />
+      <div className="text-center mt-6">
+        <h3 className="text-lg text-gray-600 mb-2">이미 계정이 있으신가요?</h3>
+        <Link to="/Login" className="text-blue-500 hover:underline">
+          로그인
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Signup;
