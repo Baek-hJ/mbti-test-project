b7ed8b53f13804b32331669c1e729c5c9f4d0d56
hj <hyeonjubaek06@gmail.com>
2025-02-25 11:02:43 +0900

    Refactor: 라우터 구성 변경 및 ProtectedRoute 추가

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,20 +1,26 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
-import Signup from "../pages/Signup";
 import Profile from "../pages/Profile";
 import Test from "../pages/Test";
 import Results from "../pages/Results";
-import Login from "../pages/login";
+import Signup from "../pages/Signup";
+import Login from "../pages/Login";
+import ProtectedRoute from "../components/ProtectedRoute"; // ProtectedRoute 추가
 
 const Router = () => {
   return (
     <Routes>
+      {/* 홈, 회원가입, 로그인 페이지는 누구나 접근 가능 */}
       <Route path="/" element={<Home />} />
-      <Route path="/signup" element={<Signup />} /> {/* 소문자로 변경 */}
+      <Route path="/signup" element={<Signup />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/results" element={<Results />} />
-      <Route path="/test" element={<Test />} />
+
+      {/* ProtectedRoute로 보호된 경로 설정 */}
+      <Route element={<ProtectedRoute />}>
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/results" element={<Results />} />
+        <Route path="/test" element={<Test />} />
+      </Route>
     </Routes>
   );
 };
